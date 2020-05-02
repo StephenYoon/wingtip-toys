47ebcb345688e42f3045fbf993ff7e4410383741
Stephen Yoon <yoonic@gmail.com>
2020-05-01 20:19:16 -0400

    Implemented main content component

--- WingTipToys.Web/ClientApp/src/app/product-catalog/main-content/main-content.component.ts
+++ WingTipToys.Web/ClientApp/src/app/product-catalog/main-content/main-content.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-main-content',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-content.component.scss']
 })
 export class MainContentComponent implements OnInit {
+  @Input() products: Product[];
+  @Input() selectedProductTypeId: number;
+  @Output() addProductId = new EventEmitter<number>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  filteredProducts(): Product[] {
+    if (!this.selectedProductTypeId) {
+      return this.products;
+    }
+
+    var filteredResults = this.products.filter(p => p.type == this.selectedProductTypeId);
+    return filteredResults;
+  }
 }
