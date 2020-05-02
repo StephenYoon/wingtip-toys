4eb580202021cdae0414e5860763f5e05c8e4fd4
Stephen Yoon <yoonic@gmail.com>
2020-05-01 20:08:30 -0400

    Added productService to HomeComponent with onProductTypeChange

--- WingTipToys.Web/ClientApp/src/app/product-catalog/home/home.component.ts
+++ WingTipToys.Web/ClientApp/src/app/product-catalog/home/home.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  selectedProductTypeId: number;
+  products$: Observable<Product[]>;
+  
+  constructor(private productService: ProductService){}
+
+  ngOnInit(): void {
+    this.products$ = this.productService.getProducts();
+  }
+
+  onProductTypeChange(typeId: number): void {
+    this.selectedProductTypeId = typeId;
+
+    /* 
+      TODO: Optionally we can request the filtered data from the API backend
+            if the requested data becomes large.
+      */
+    // if (typeId === 0) {
+    //   this.products$ = this.productService.getProducts();
+    // } else {
+    //   this.products$ = this.productService.getProductsByType(typeId);
+    // }
+  }
 }
