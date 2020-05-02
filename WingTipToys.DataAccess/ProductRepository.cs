4622af74a35a143e1116b3e6f6c2b7507534bf7b
Stephen Yoon <yoonic@gmail.com>
2020-05-01 20:52:07 -0400

    Filled in the ClientApp's shopping-cart service

--- WingTipToys.DataAccess/ProductRepository.cs
+++ WingTipToys.DataAccess/ProductRepository.cs
@@ -39,13 +39,11 @@ namespace WingTipToys.DataAccess
 
         private IEnumerable<Product> MockData()
         {
-            var productIdCounter = 0;
-
             var data = new List<Product>()
             {
                 new Product 
                 { 
-                    Id = productIdCounter++, 
+                    Id = 1, 
                     Name = "Chevy Camaro SS toy", 
                     UnitPrice = 25.99m, 
                     ImageUrl = "https://static.summitracing.com/global/images/prod/xlarge/rla-aw243_uu_xl.jpg", 
@@ -53,7 +51,7 @@ namespace WingTipToys.DataAccess
                 },
                 new Product 
                 { 
-                    Id = productIdCounter++,
+                    Id = 2,
                     Name = "Ford Mustang 5.0 toy", 
                     UnitPrice = 20.99m, 
                     ImageUrl = "https://images-na.ssl-images-amazon.com/images/I/61kRyEKfzUL._AC_SX425_.jpg", 
@@ -61,7 +59,7 @@ namespace WingTipToys.DataAccess
                 },
                 new Product 
                 { 
-                    Id = productIdCounter++,
+                    Id = 3,
                     Name = "Dodge Challender Hellcat toy", 
                     UnitPrice = 28.99m, 
                     ImageUrl = "https://www.dhresource.com/0x0/f2/albu/g8/M01/4E/11/rBVaVFyVormAGBQWAAEUtQEx870249.jpg", 
@@ -69,7 +67,7 @@ namespace WingTipToys.DataAccess
                 },
                 new Product
                 {
-                    Id = productIdCounter++,
+                    Id = 4,
                     Name = "Jeep Wrangler Rubicon",
                     UnitPrice = 67370.00m,
                     ImageUrl = "https://www.wallpaperup.com/uploads/wallpapers/2014/07/10/393467/f81e3e63d16076d51688e9802c573f79.jpg",
@@ -77,7 +75,7 @@ namespace WingTipToys.DataAccess
                 },
                 new Product
                 {
-                    Id = productIdCounter++,
+                    Id = 5,
                     Name = "Toyota Supra",
                     UnitPrice = 63450.00m,
                     ImageUrl = "https://cnet2.cbsistatic.com/img/rZMNDzGaH70YeWkRQrTfPl_H--Y=/940x0/2020/02/12/99823121-7ef6-4d35-b781-ef97aa11e5ff/2021-toyota-supra-2-0-promo.jpg",
@@ -85,7 +83,7 @@ namespace WingTipToys.DataAccess
                 },
                 new Product 
                 { 
-                    Id =productIdCounter++,
+                    Id = 6,
                     Name = "Tennis racket", 
                     UnitPrice = 50.99m, 
                     ImageUrl = "https://www.e-tennis.com/media/catalog/product/cache/1/thumbnail/800x800/9df78eab33525d08d6e5fb8d27136e95/w/r/wr014011_wilson-blade-100l-v7-0-tennis-racquet.jpg", 
@@ -93,7 +91,7 @@ namespace WingTipToys.DataAccess
                 },
                 new Product 
                 { 
-                    Id = productIdCounter++,
+                    Id = 7,
                     Name = "Tennis balls", 
                     UnitPrice = 3.99m, 
                     ImageUrl = "https://www.gophersport.com/cmsstatic/img/453/G-51109-PressurelessTennisBalls-ce-3-clean.jpg?medium", 
@@ -101,7 +99,7 @@ namespace WingTipToys.DataAccess
                 },
                 new Product
                 {
-                    Id = productIdCounter++,
+                    Id = 8,
                     Name = "Bang: The Dice Game", 
                     UnitPrice = 15.99m, 
                     ImageUrl = "https://media.miniaturemarket.com/media/catalog/product/d/v/dvg9112_2.jpg", 
@@ -109,7 +107,7 @@ namespace WingTipToys.DataAccess
                 },
                 new Product
                 { 
-                    Id = productIdCounter++,
+                    Id = 9,
                     Name = "Avalon",
                     UnitPrice = 12.99m, 
                     ImageUrl = "https://cdn.shopify.com/s/files/1/1704/1809/products/6a9be3ba0badcb7369aa8a6e4b746048fc23334d_500x.jpg?v=1559261133", 
@@ -117,7 +115,7 @@ namespace WingTipToys.DataAccess
                 },
                 new Product 
                 { 
-                    Id = productIdCounter++,
+                    Id = 10,
                     Name = "Battleship", 
                     UnitPrice = 7.99m, 
                     ImageUrl = "https://cf.geekdo-images.com/imagepage/img/nk5MSBn_TZs-vTtu2KrY2xWBfok=/fit-in/900x600/filters:no_upscale()/pic1293565.jpg", 
--- WingTipToys.Web/ClientApp/src/app/services/shopping-cart.service.ts
+++ WingTipToys.Web/ClientApp/src/app/services/shopping-cart.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 import { ShoppingCart } from '../models/shopping-cart';
 import { v4 as uuidv4 } from 'uuid';
+import { take, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -32,14 +33,28 @@ export class ShoppingCartService {
 
   getShoppingCart(): BehaviorSubject<ShoppingCart> {
     
-    // TODO: ...
+    this.http.get<ShoppingCart>(`${this.apiPath}/${this.shoppingCartId}`)
+    .pipe(
+      tap((data) => console.info(`Retrieved shopping cart ${this.shoppingCartId}. ` + JSON.stringify(data,null,2))),
+      take(1)
+    )
+    .subscribe((shoppingCart: ShoppingCart) => {
+      this.shoppingCart$.next(shoppingCart);
+    });
 
     return this.shoppingCart$;
   }
   
   addToShoppingCart(productId: number): BehaviorSubject<ShoppingCart> {
     
-    // TODO: ...
+    this.http.post<ShoppingCart>(`${this.apiPath}/${this.shoppingCartId}`, productId)
+    .pipe(
+      tap((data) => console.info(`Added product ${productId} to shopping cart ${this.shoppingCartId}.` + JSON.stringify(data,null,2))),
+      take(1)
+    )
+    .subscribe((shoppingCart: ShoppingCart) => {
+      this.shoppingCart$.next(shoppingCart);
+    });
     
     return this.shoppingCart$;
   }
